337c087e650731e23131c45fcb122a9f6ac822d2
Mayte <mayte710@hotmail.com>
2024-02-16 12:46:40 +0000

    back and front

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <Button variant="primary">Botón de Bootstrap</Button>
+    </div>
+  );
+}
+export default App;
